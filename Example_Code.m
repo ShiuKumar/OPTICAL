b0b7aa858412a1f8ef0afb17a1f21748f526a242
Shiu Kumar <32100156+ShiuKumar@users.noreply.github.com>
2019-10-01 14:07:37 +1200

    Update Example_Code.m

--- Example_Code.m
+++ Example_Code.m
@@ -1,8 +1,10 @@
-load('sample_data')
-load('Actual_class')
-ind = crossvalind('Kfold',Actual_class,10);
+load('sample_data') % load sample data
+load('Actual_class') % load the actual class of the sample data
+ind = crossvalind('Kfold',Actual_class,10); % generate indices to divide 
+% sample data into train and test data
 
-num = 1;
+% Select 1st set as test data and remaining sets as test data
+num = 1; 
 
 test = (ind == num); 
 test_ind = find(test == 1);
@@ -14,4 +16,6 @@ Target_Train = Actual_class(train_ind);
 Test_data = sample_data(:,:,test_ind);
 Target_Test = Actual_class(test_ind);
 
+% Researchers can provide their own data and parameters to the OPTICAL
+% function. Here window size of 50 sample points with 20% overlap is used
 predicted_class = OPTICAL(Train_data,Test_data,Target_Train, Target_Test, 50, 20);
